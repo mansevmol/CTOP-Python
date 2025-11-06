ed9558a69fd3bfaa12d18a74cfbb1ffdc99397cf
mansevmol <alu-mansevmol@foc.es>
2025-11-06 18:05:28 +0100

    06/11/2025 clase

---
+++ Unidad03/ej4.py
@@ -0,0 +1,4 @@
+alumno = {"nombre":"Manu" ,"edad":89 ,"curso": "Primero"}
+
+for clave , valor in alumno.items():
+  print(clave,valor)
---
+++ Unidad03/ej5.py
@@ -0,0 +1,11 @@
+edad = int(input("dime la edad "))
+
+
+if(edad < 18):
+  print("eres menor de edad")
+
+elif(edad>=18 and edad<=64):
+  print("eres adulto")
+
+else:
+  print("eres mayor")
